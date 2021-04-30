1e0583748d0f2e0d15e0260592809f19e27e4112
Ekaterina Skobeleva <Veritable38210@yandex.ru>
2021-04-30 17:37:17 +0300

    modified test for AverageSale

---
+++ src/test/java/ru/netology/stats/AverageSalesAmountTest.java
@@ -0,0 +1,10 @@
+package ru.netology.stats;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AverageSalesAmountTest {
+
+    @org.junit.jupiter.api.Test
+    void calculate() {
+    }
+}
